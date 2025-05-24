712df2ab9350af77be41f1f6b1638d3e5f16b21d
Aidan Finnegan <71090977+ABFCode@users.noreply.github.com>
2025-05-23 21:39:35 -0400

    successfully pulled our PR

--- server/CodeReviewBot/src/main/java/com/github/abfcode/codereviewbot/CodeReviewBotApplication.java
+++ server/CodeReviewBot/src/main/java/com/github/abfcode/codereviewbot/CodeReviewBotApplication.java
@@ -1,13 +1,38 @@
 package com.github.abfcode.codereviewbot;
 
+import com.github.abfcode.codereviewbot.service.BitbucketApiService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodeReviewBotApplication {
+@Slf4j
+@RequiredArgsConstructor
+public class CodeReviewBotApplication implements CommandLineRunner {
+
+    private final BitbucketApiService bitbucketApiService;
 
     public static void main(String[] args) {
         SpringApplication.run(CodeReviewBotApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        String testPullRequestId = "1";
+
+        if(args.length > 0) {
+            testPullRequestId = args[0];
+            log.info("Running with pull request ID {}", testPullRequestId);
+        }
+        else{
+            log.info("Running with default pull request ID {}", testPullRequestId);
+        }
+
+        log.info("Attempting to fetch PR details for ID {}", testPullRequestId);
+        String response = bitbucketApiService.getRawPRDetails(testPullRequestId);
+        log.info("\n--- RAW BITBUCKET PULL REQUEST DETAILS ---\n{}", response);
+        log.info("--- END OF RAW RESPONSE ---");
+    }
 }
--- server/CodeReviewBot/src/main/java/com/github/abfcode/codereviewbot/service/BitbucketApiService.java
+++ server/CodeReviewBot/src/main/java/com/github/abfcode/codereviewbot/service/BitbucketApiService.java
@@ -1,8 +1,43 @@
 package com.github.abfcode.codereviewbot.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class BitbucketApiService {
+
+    private final RestClient restClient;
+
+    @Value("${bitbucket.workspace}")
+    private String workspace;
+
+    @Value("${bitbucket.reposlug}")
+    private String repo;
+
+    public String getRawPRDetails(String pullRequestId) {
+        String path = String.format("/repositories/%s/%s/pullrequests/%s", workspace, repo, pullRequestId);
+
+        log.info("Getting raw PR details from Bitbucket API at {}", path);
+        log.info("Using workspace {}, repo {}", workspace, repo);
+
+        try{
+            String responseBody = restClient.get()
+                    .uri(path)
+                    .retrieve()
+                    .body(String.class);
+            log.info("Got raw PR details from Bitbucket API");
+            return responseBody;
+        }
+        catch (RestClientException e) {
+            log.error("Error fetching PR details for ID {}: {}", pullRequestId, e.getMessage(), e);
+            return "Error: Could not fetch PR details." + e.getMessage();
+        }
+    }
 
 }
