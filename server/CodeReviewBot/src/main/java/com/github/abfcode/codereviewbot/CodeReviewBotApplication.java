0d9a8bf9d3a29985eb4536bb4f7eb3b21da12a43
Aidan Finnegan <71090977+ABFCode@users.noreply.github.com>
2025-05-26 18:29:02 -0400

    removed clr, added get endpoint for context

--- server/CodeReviewBot/src/main/java/com/github/abfcode/codereviewbot/CodeReviewBotApplication.java
+++ server/CodeReviewBot/src/main/java/com/github/abfcode/codereviewbot/CodeReviewBotApplication.java
@@ -1,45 +1,13 @@
 package com.github.abfcode.codereviewbot;
 
-import com.github.abfcode.codereviewbot.dto.PullRequestDTO;
-import com.github.abfcode.codereviewbot.service.BitbucketApiService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
-@RequiredArgsConstructor
-public class CodeReviewBotApplication implements CommandLineRunner {
-
-    private final BitbucketApiService bitbucketApiService;
+public class CodeReviewBotApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(CodeReviewBotApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        String testPullRequestId = "1";
-
-        if(args.length > 0) {
-            testPullRequestId = args[0];
-            log.info("Running with pull request ID {}", testPullRequestId);
-        }
-        else{
-            log.info("Running with default pull request ID {}", testPullRequestId);
-        }
-
-        log.info("Attempting to fetch PR details for ID {}", testPullRequestId);
-        PullRequestDTO pullRequest = bitbucketApiService.getPullRequestDetails(testPullRequestId);
-
-        if (pullRequest != null) {
-            log.info("\n--- DESERIALIZED PULL REQUEST DTO ---\n{}", pullRequest.toString());
-            log.info("PR Title: {}", pullRequest.title());
-            log.info("PR Author: {}", pullRequest.author() != null ? pullRequest.author().displayName() : "N/A");
-
-        }
-        log.info("--- END OF DTO TEST ---");
-    }
 }
--- server/CodeReviewBot/src/main/java/com/github/abfcode/codereviewbot/controller/ContextController.java
+++ server/CodeReviewBot/src/main/java/com/github/abfcode/codereviewbot/controller/ContextController.java
@@ -1,10 +1,25 @@
 package com.github.abfcode.codereviewbot.controller;
 
+import com.github.abfcode.codereviewbot.service.BitbucketApiService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequestMapping("/context")
+@RequiredArgsConstructor
 public class ContextController {
+
+    private final BitbucketApiService bitbucketApiService;
+
+    @GetMapping("/pr/{pullRequestId}")
+    public ResponseEntity<String> getPullRequestDetails(@PathVariable String pullRequestId) {
+        return ResponseEntity.ok(bitbucketApiService.getPullRequestDetails(pullRequestId).toString());
+    }
 
 }
